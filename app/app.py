97acdd3d2858bbf393f8a3772314db1c1129d031
Sarang Kharche <sarangkharche@gmail.com>
2022-08-04 23:32:34 +0000

    added " instead of '

--- app/app.py
+++ app/app.py
@@ -28,12 +28,12 @@ hashed_passwords = stauth.hasher(passwords).generate()
 authenticator = stauth.authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
 
 if authentication_status:
- st.write(‘Welcome *%s*’ % (name))
+ st.write("Welcome *%s*" % (name))
  # your application
 elif authentication_status == False:
- st.error(‘Username/password is incorrect’)
+ st.error("Username/password is incorrect")
 elif authentication_status == None:
- st.warning(‘Please enter your username and password’)
+ st.warning("Please enter your username and password")
 
 
 
