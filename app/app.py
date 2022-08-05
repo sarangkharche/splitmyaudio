e857dfe9328124d77ffd9b559fcf8547ab35f7aa
Sarang Kharche <sarangkharche@gmail.com>
2022-08-05 00:28:19 +0000

    undo

--- app/app.py
+++ app/app.py
@@ -5,9 +5,6 @@ from typing import Generator, List
 import streamlit as st
 from spleeter.separator import Codec
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
-
 st.set_page_config(
      page_title="Split My Audio",
      page_icon="app/media/favicon_io/favicon-32x32.png",
